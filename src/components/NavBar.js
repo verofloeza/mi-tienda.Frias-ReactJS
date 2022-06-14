33a3f381b3d2245c6e94f0eec06357eddb35a893
Vero <verofloeza@gmail.com>
2022-06-14 15:10:31 -0300

    menu

---
+++ src/components/NavBar.js
@@ -0,0 +1,42 @@
+import './css/NavBar.css';
+import 'bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+import {Col, Container, Row} from 'react-bootstrap';
+import { faSearch, faStore } from "@fortawesome/free-solid-svg-icons";
+
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import React from 'react';
+
+function NavBar() {
+  return (
+    <header className='header'>
+      <Container>
+      <Row>
+        <Col>
+          <img src={require('../assets/images/logo-mitienda-300px.png')} className="logoEmpresa" />
+        </Col>
+        <Col xs={6} className='menu'>
+          <ul>
+            <li><a src="#">Inicio</a></li>
+            <li><a src="#">Productos</a></li>
+            <li><a src="#">Nosotros</a></li>
+            <li><a src="#">Contacto</a></li>
+          </ul>
+        </Col>
+        <Col>
+          <a src="#" className='icons'>
+            <FontAwesomeIcon icon={faSearch} />
+          </a>
+          <a src="#" className='icons'>
+            <FontAwesomeIcon icon={faStore} />
+          </a>
+          
+        </Col>
+      </Row>
+      </Container>  
+    </header>
+  );
+}
+
+export default NavBar;
